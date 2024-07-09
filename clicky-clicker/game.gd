0b56e783991798a9edbdd5b5ae2bb66004db922c
Max Aller <nanodeath@gmail.com>
2024-07-08 20:30:21 -0700

    Add random ball and fixing resolution stuff.

--- clicky-clicker/game.gd
+++ clicky-clicker/game.gd
@@ -1,7 +1,12 @@
 class_name Game extends Control
 
+## Seconds before the ball starts to decay.
+@export_range(1, 20) var ball_lifetime: float
+
 @onready var click_button: Button = $VBoxContainer/ClickButton
 @onready var counter_label: RichTextLabel = $VBoxContainer/CounterLabel
+
+const BALL = preload("res://ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +24,22 @@ func _button_pressed():
 	# Also increment the counter text
 	var counter_value: = counter_label.text.to_int()
 	counter_label.text = str(counter_value + 1)
+	
+	# Create a random "ball" for the lulz
+	var ball := BALL.instantiate()
+	var velocity := Vector2(1000, 0).rotated(randf() * 2 * PI)
+	ball.linear_velocity = velocity
+	add_child(ball)
+	ball.global_position = get_global_mouse_position()
+	
+	# Fade the ball out to a random transparent color after a while
+	var timer := get_tree().create_timer(ball_lifetime)
+	timer.timeout.connect(func(): 
+		var tween = get_tree().create_tween()
+		var color := Color(randf(), randf(), randf(), 0)
+		tween.tween_property(ball, "modulate", color, 1)
+		tween.tween_callback(func(): ball.queue_free())
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
