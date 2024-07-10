6bcccbe93fc8919e1c5234e47e2b9d6fb876f6fd
Max Aller <nanodeath@gmail.com>
2024-07-09 18:44:08 -0700

    Exit when Escape is pressed.
    Might not do anything on web, but is handy during development.

--- clicky-clicker/game.gd
+++ clicky-clicker/game.gd
@@ -58,4 +58,5 @@ func _update_current_ball_count():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
